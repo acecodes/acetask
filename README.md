9a744979fa73d44a5c4b127041b0b4d7dee91efd
Ace <ace.github@gmail.com>
2014-10-06 12:12:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# AceTask
+---
+
+Keep track of what you're doing with this simple Angular.js app from [52 Aces](http://www.52aces.com). See the live version at [www.52aces.com/acetask/](http://www.52aces.com/acetask/).
---
+++ js/acetask.js
@@ -0,0 +1,22 @@
+var app = angular.module('Todo', []);
+
+app.controller('TodoCtrl', function($scope) {
+	$scope.todos = [
+		'Add your tasks to this list'
+	];
+
+	$scope.done = function(todo) {
+		var indexOf = $scope.todos.indexOf(todo);
+		if (indexOf !== -1) {
+			$scope.todos.splice(indexOf, 1, 'Task completed');
+		}
+	};
+
+	$scope.add = function(e) {
+		if (e.which && e.which === 13) {
+			$scope.todos.push($scope.newTodo);
+			$scope.newTodo = '';
+		}
+	};
+  
+});
