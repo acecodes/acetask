f5e4ab2f875a42fba4c41f5ec091fe59f08d54fd
Ace <ace.github@gmail.com>
2014-10-27 17:22:54 -0700

    Still trying to tackle this damn completed task list...

--- js/acetask.js
+++ js/acetask.js
@@ -1,19 +1,23 @@
-var app = angular.module('Todo', []);
+var app = angular.module('AceTask', []);
 
-app.controller('TodoCtrl', function($scope) {
+app.controller('AceTaskCtrl', function($scope) {
 	$scope.todos = [];
 	$scope.finished = [];
 	$scope.count = 0;
 
 	$scope.done = function(todo) {
-		var new_time = new Date();
-		var timestamp = new_time.getHours() + ":" + new_time.getMinutes();
+		var timestamp = new Date();
+
+		var task = {text:todo,
+					time:timestamp.getHours() + ":" + timestamp.getMinutes()};
+
 		var indexOf = $scope.todos.indexOf(todo);
 		if (indexOf !== -1) {
 			$scope.todos.splice(indexOf, 1);
 		}
 		$scope.count += 1;
-		$scope.finished.push(todo + " (" + timestamp + ")");
+
+		$scope.finished.push(task.text + " (" + task.time + ")");
 		
 	};
 
