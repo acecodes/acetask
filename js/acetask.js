a467b0d031e3714814011153a82cbe0d7e3331e9
Ace <ace.github@gmail.com>
2015-04-12 20:28:48 -0700

    Cleaned up index and completed task list

--- js/acetask.js
+++ js/acetask.js
@@ -1,31 +1,33 @@
 var app = angular.module('AceTask', []);
 
 app.controller('AceTaskCtrl', function($scope) {
-	$scope.todos = [];
-	$scope.finished = [];
-	$scope.count = 0;
+    $scope.todos = [];
+    $scope.finished = [];
+    $scope.count = 0;
 
-	$scope.done = function(todo) {
-		var timestamp = new Date();
+    $scope.done = function(todo) {
+        var timestamp = new Date();
 
-		var task = {text:todo,
-					time:timestamp.getHours() + ":" + timestamp.getMinutes()};
+        var task = {
+            text: todo,
+            time: timestamp.getHours() + ':' + timestamp.getMinutes()
+        };
 
-		var indexOf = $scope.todos.indexOf(todo);
-		if (indexOf !== -1) {
-			$scope.todos.splice(indexOf, 1);
-		}
-		$scope.count += 1;
+        var indexOf = $scope.todos.indexOf(todo);
+        if (indexOf !== -1) {
+            $scope.todos.splice(indexOf, 1);
+        }
+        $scope.count += 1;
 
-		$scope.finished.push(task.text + " (" + task.time + ")");
-		
-	};
+        $scope.finished.push(task.text + ' (' + task.time + ')');
 
-	$scope.add = function(e) {
-		if (e.which && e.which === 13) {
-			$scope.todos.push($scope.newTodo);
-			$scope.newTodo = '';
-		}
-	};
-  
+    };
+
+    $scope.add = function(e) {
+        if (e.which && e.which === 13) {
+            $scope.todos.push($scope.newTodo);
+            $scope.newTodo = '';
+        }
+    };
+
 });
